d9c1edc50a6bb5deeba84e216d7a515414dccc26
autierg <autiergilles@gmail.com>
2016-03-01 11:29:31 +0100

    tyu

---
+++ app/src/main/java/acy/iut/theviborationuniversity/ProfileAutreEtudiant.java
@@ -0,0 +1,13 @@
+package acy.iut.theviborationuniversity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by autierg on 01/03/2016.
+ */
+public class ProfileAutreEtudiant extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+
+    }
+}
